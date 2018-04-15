437082e4f046d7ef56111d2f3a341d224f7c5a49
Aziz <azizbohra07@gmail.com>
2018-04-15 09:26:53 +0300

    aziz

--- config.php
+++ config.php
@@ -7,7 +7,7 @@ define('BASE_URL', filter_var('http://localhost/websolutionspoint/login-via-goog
 
 // For Google
 // oauth2_client_id, oauth2_client_secret, and to register your oauth2_redirect_uri.
-define('CLIENT_ID', '654483866262-i6ooodat56ib4frtt7dg3r8apmte0qcn.apps.googleusercontent.com');
+define('CLIENT_ID', '654483866161-i6ooodat56ib4frtt7dg3r8apmte0qcn.apps.googleusercontent.com');
 define('CLIENT_SECRET', 'GOqI_1qiGyiJ4Absn-rzbWR7');
 define('REDIRECT_URI_GOOGLE', 'http://localhost/websolutionspoint/login-via-google-in-php/login.php?google'); //example:http://localhost/social/login.php?google,http://example/login.php?google
 define('APPROVAL_PROMPT', 'auto');
